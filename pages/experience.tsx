f3a46c8632f0e109b65e22b013c4a0cbbb288cef
shantanu <i.am.shantanu07@gmail.com>
2025-12-06 18:41:02 +0530

    Abbreviate Chief Technology Officer to CTO

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -10,7 +10,7 @@ const ExperiencePage = () => {
 
 ---
 
-## [Work Experience] Product Developer • Chief Technology Officer @ Grosity
+## [Work Experience] Product Developer • CTO @ Grosity
 **Timeline:** October 2025 - Present  
 **Status:** Active
 
