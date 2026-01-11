33be84e0c2e6cb4cfae2b015c7d3057ddab2cbb2
shantanu <i.am.shantanu07@gmail.com>
2026-01-12 01:56:19 +0530

    Fix theme flash on page load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 import Layout from '@/components/Layout';
 import Head from '@/components/Head';
@@ -18,18 +19,35 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <ClickSpark
-      sparkColor='#fff'
-      sparkSize={10}
-      sparkRadius={15}
-      sparkCount={8}
-      duration={400}
-    >
-      <Layout>
-        <Head title={`Shantanu | ${pageProps.title}`} />
-        <Component {...pageProps} />
-      </Layout>
-    </ClickSpark>
+    <>
+      <Script
+        id="theme-script"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function() {
+              const theme = localStorage.getItem('theme') || 'ayu-dark';
+              document.documentElement.setAttribute('data-theme', theme);
+              if (!localStorage.getItem('theme')) {
+                localStorage.setItem('theme', 'ayu-dark');
+              }
+            })();
+          `,
+        }}
+      />
+      <ClickSpark
+        sparkColor='#fff'
+        sparkSize={10}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={400}
+      >
+        <Layout>
+          <Head title={`Shantanu | ${pageProps.title}`} />
+          <Component {...pageProps} />
+        </Layout>
+      </ClickSpark>
+    </>
   );
 }
 
