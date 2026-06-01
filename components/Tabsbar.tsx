60dd671b541fb028a0469bd7076577660003527e
shantanu <i.am.shantanu07@gmail.com>
2026-06-01 13:52:28 +0530

    refactor: simplify Tabsbar to show only recent tabs instead of folder-based tabs

--- components/Tabsbar.tsx
+++ components/Tabsbar.tsx
@@ -1,8 +1,7 @@
 import Tab from '@/components/Tab';
-import { useFolderContext } from '@/contexts/FolderContext';
 import { useRecentTabs } from '@/hooks/useRecentTabs';
 import { motion, AnimatePresence } from 'framer-motion';
-import { rootFile, portfolioFiles, navFolders } from '@/data/navigation';
+import { rootFile } from '@/data/navigation';
 
 import styles from '@/styles/Tabsbar.module.css';
 
@@ -13,33 +12,7 @@ const tabVariants = {
 };
 
 const Tabsbar = () => {
-  const { 
-    portfolioOpen, 
-    developmentOpen, 
-    skillsOpen, 
-    careerOpen, 
-    researchOpen, 
-    resumeOpen 
-  } = useFolderContext();
   const { tabs, closeTab } = useRecentTabs();
-
-  // Get the appropriate open state for each folder
-  const getFolderOpen = (folderId: string) => {
-    switch (folderId) {
-      case 'development':
-        return developmentOpen;
-      case 'skills':
-        return skillsOpen;
-      case 'career':
-        return careerOpen;
-      case 'research':
-        return researchOpen;
-      case 'resume':
-        return resumeOpen;
-      default:
-        return false;
-    }
-  };
 
   return (
     <div className={styles.tabs}>
@@ -54,59 +27,9 @@ const Tabsbar = () => {
         <Tab icon={rootFile.icon} filename={rootFile.name} path={rootFile.path} />
       </motion.div>
 
-      {/* Portfolio folder files */}
-      <AnimatePresence>
-        {portfolioOpen && (
-          <>
-            {portfolioFiles.map((file) => (
-              <motion.div
-                key={file.name}
-                variants={tabVariants}
-                initial="initial"
-                animate="animate"
-                exit="exit"
-                className={styles.tabItem}
-              >
-                <Tab icon={file.icon} filename={file.name} path={file.path} />
-              </motion.div>
-            ))}
-          </>
-        )}
-      </AnimatePresence>
-
-      {/* Dynamic folder files */}
-      <AnimatePresence>
-        {portfolioOpen && navFolders.map((folder) => {
-          const folderOpen = getFolderOpen(folder.id);
-          return folderOpen ? folder.files.map((file) => (
-            <motion.div
-              key={file.name}
-              variants={tabVariants}
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              className={styles.tabItem}
-            >
-              <Tab icon={file.icon} filename={file.name} path={file.path} />
-            </motion.div>
-          )) : null;
-        })}
-      </AnimatePresence>
-
       {/* Recent tabs */}
       <AnimatePresence>
-        {tabs.filter(tab => {
-          const alreadyShown = [
-            '/',
-            ...(portfolioOpen ? portfolioFiles.map(f => f.path) : []),
-            ...(portfolioOpen && developmentOpen ? navFolders.find(f => f.id === 'development')?.files.map(f => f.path) ?? [] : []),
-            ...(portfolioOpen && skillsOpen ? navFolders.find(f => f.id === 'skills')?.files.map(f => f.path) ?? [] : []),
-            ...(portfolioOpen && careerOpen ? navFolders.find(f => f.id === 'career')?.files.map(f => f.path) ?? [] : []),
-            ...(portfolioOpen && researchOpen ? navFolders.find(f => f.id === 'research')?.files.map(f => f.path) ?? [] : []),
-            ...(portfolioOpen && resumeOpen ? navFolders.find(f => f.id === 'resume')?.files.map(f => f.path) ?? [] : []),
-          ];
-          return !alreadyShown.includes(tab.path);
-        }).map(tab => (
+        {tabs.map(tab => (
           <motion.div
             key={`recent-${tab.path}`}
             variants={tabVariants}
