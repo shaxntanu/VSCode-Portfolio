7a5cb0f24142c36973d082b57a5542ea0d700083
shantanu <i.am.shantanu07@gmail.com>
2026-08-02 10:41:15 +0530

    fix: update deprecated tsparticles dependencies to @tsparticles packages

--- components/ParticlesBackground.tsx
+++ components/ParticlesBackground.tsx
@@ -1,7 +1,7 @@
 import { useCallback } from "react";
-import Particles from "react-tsparticles";
-import type { Engine } from "tsparticles-engine";
-import { loadSlim } from "tsparticles-slim";
+import Particles from "@tsparticles/react";
+import type { Engine } from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
 
 const ParticlesBackground = () => {
   const particlesInit = useCallback(async (engine: Engine) => {
